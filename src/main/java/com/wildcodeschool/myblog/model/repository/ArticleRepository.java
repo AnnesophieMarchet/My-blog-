ab6985aa5056c2867272d4a6599771c9c7e8c80d
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2025-01-14 14:24:36 +0100

    Spring 04 - Requêtes Personnalisées

--- src/main/java/com/wildcodeschool/myblog/model/repository/ArticleRepository.java
+++ src/main/java/com/wildcodeschool/myblog/model/repository/ArticleRepository.java
@@ -3,8 +3,19 @@ package com.wildcodeschool.myblog.model.repository;
 import com.wildcodeschool.myblog.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
 
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    List<Article> findByTitle(String title);
+
+    List<Article> findByContent(String content);
+
+    List<Article> findByContentContaining(String content);
+
+    List<Article> findByCreatedAtAfter(LocalDateTime createdAt);
+
+    List<Article> findTop5ByOrderByCreatedAtDesc();
 }
